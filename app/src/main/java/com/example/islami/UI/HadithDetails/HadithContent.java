57671b299280b1f70a77851624b569dbaf6b6045
MGTC <markraouf2001@gmail.com>
2025-01-21 21:10:20 +0200

    fix: recovered except HadithDetailsActivity

--- app/src/main/java/com/example/islami/UI/HadithDetails/HadithContent.java
+++
@@ -1,6 +0,0 @@
-package com.example.islami.UI.HadithDetails;
-
-import android.app.Activity;
-
-public class HadithContent extends Activity {
-}
--- app/src/main/java/com/example/islami/UI/HadithDetails/HadithDetailsActivity.kt
+++ app/src/main/java/com/example/islami/UI/HadithDetails/HadithDetailsActivity.kt
@@ -1,16 +1,31 @@
 package com.example.islami.UI.HadithDetails
 
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.islami.UI.home.hadith.HadethDataClass
 import com.example.islami.databinding.ActivityHadithDetailsBinding
 
 class HadithDetailsActivity : AppCompatActivity() {
     lateinit var binding: ActivityHadithDetailsBinding
+    var hadeth: HadethDataClass? = null
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         binding = ActivityHadithDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initviews()
+        binding.hadithTitle.text = hadeth?.title
+        binding.content.hadithcontent.text = hadeth?.content
+
+    }
+
+    private fun initviews() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            hadeth = intent.getParcelableExtra(Extra_hadithDetails, HadethDataClass::class.java)
+        }
+
+
     }
 
     companion object {
