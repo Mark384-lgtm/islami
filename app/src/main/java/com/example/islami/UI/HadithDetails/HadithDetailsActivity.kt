f6b70d94e76b2d5d25f108296ea7fffd007e82f0
MGTC <markraouf2001@gmail.com>
2025-01-21 21:19:10 +0200

    fix:recovered remaining BackButton in HadithDetailsActivity

--- app/src/main/java/com/example/islami/UI/HadithDetails/HadithDetailsActivity.kt
+++ app/src/main/java/com/example/islami/UI/HadithDetails/HadithDetailsActivity.kt
@@ -2,7 +2,6 @@ package com.example.islami.UI.HadithDetails
 
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.islami.UI.home.hadith.HadethDataClass
 import com.example.islami.databinding.ActivityHadithDetailsBinding
@@ -10,8 +9,8 @@ import com.example.islami.databinding.ActivityHadithDetailsBinding
 class HadithDetailsActivity : AppCompatActivity() {
     lateinit var binding: ActivityHadithDetailsBinding
     var hadeth: HadethDataClass? = null
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityHadithDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initviews()
